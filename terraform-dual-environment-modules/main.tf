0b9c770c50d0ad9eb137291c7362b6f3ccaedfcd
Adam Cavaliere <adam@mbp.local>
2018-03-13 10:01:19 -0500

    modified rules

--- terraform-dual-environment-modules/main.tf
+++ terraform-dual-environment-modules/main.tf
@@ -29,7 +29,7 @@ module "security_group" {
   vpc_id      = "${module.vpc.vpc_id}"
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  ingress_rules       = ["http-80-tcp", "all-icmp","ssh-tcp"]
   #ssh-tcp
   egress_rules        = ["all-all"]
   tags = {
