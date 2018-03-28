d5add3dfe2923abe1709abc2a0ecb396909965a4
AdamCavaliere <adam.cavaliere@gmail.com>
2018-03-28 09:29:34 -0400

    added network IP

--- terraform-azure/network.tf
+++ terraform-azure/network.tf
@@ -17,7 +17,7 @@ resource "azurerm_network_interface" "netint" {
   name                = "networkinterface-${count.index + 1}"
   location            = "${azurerm_resource_group.resource_gp.location}"
   resource_group_name = "${azurerm_resource_group.resource_gp.name}"
-  count = 2
+  count = 3
   ip_configuration {
     name                          = "ipconfig-${count.index + 1}"
     subnet_id                     = "${element(azurerm_subnet.test.*.id, count.index)}"
