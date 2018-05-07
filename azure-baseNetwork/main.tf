0239e6964e2bb01001d3c5f6ce91c04ca3e480c8
AdamCavaliere <adam.cavaliere@gmail.com>
2018-05-07 13:43:41 -0500

    Update main.tf

--- azure-baseNetwork/main.tf
+++ azure-baseNetwork/main.tf
@@ -14,5 +14,5 @@ resource "azurerm_subnet" "main" {
   name                 = "mainsubn"
   resource_group_name  = "${azurerm_resource_group.resource_gp.name}"
   virtual_network_name = "${azurerm_virtual_network.network.name}"
-  address_prefix       = "10.0.2.0/24"
+  address_prefix       = "10.0.3.0/24"
 }
