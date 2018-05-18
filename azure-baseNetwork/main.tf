dbeb937f07035b6db918afaf535334dafaeaccf5
AdamCavaliere <adam.cavaliere@gmail.com>
2018-05-17 22:17:49 -0500

    Update main.tf

--- azure-baseNetwork/main.tf
+++ azure-baseNetwork/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "resource_gp" {
-  name     = "${var.app_name}-rg"
+  name     = "${var.app_name}"
   location = "${var.location}"
 }
 
 resource "azurerm_virtual_network" "network" {
-  name                = "${var.app_name}-network"
+  name                = "${var.app_name}"
   address_space       = ["10.0.0.0/16"]
   location            = "${azurerm_resource_group.resource_gp.location}"
   resource_group_name = "${azurerm_resource_group.resource_gp.name}"
