b71922441d3b9ca49b5b6f433dcfa62649353f7b
AdamCavaliere <adam.cavaliere@gmail.com>
2018-08-21 09:15:16 -0500

    Update main.tf

--- azure_ApplicationConfig/main.tf
+++ azure_ApplicationConfig/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_virtual_machine" "app_vm" {
   location                      = "${azurerm_resource_group.resource_gp.location}"
   resource_group_name           = "${azurerm_resource_group.resource_gp.name}"
   network_interface_ids         = ["${element(azurerm_network_interface.netint.*.id, count.index)}"]
-  vm_size                       = "Standard_DS1_v2"
+  vm_size                       = "Standard_DS1_v3"
   count                         = "${var.instance_count}"
   delete_os_disk_on_termination = true
 
